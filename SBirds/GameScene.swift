e458b6ccd3517b3fcb12bd1932a77435f7fb9e51
justin <121174634@qq.com>
2022-11-13 09:58:28 +0800

    场景搭建

---
+++ SBirds/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  SBirds
+//
+//  Created by justin zhang on 2022/11/12.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func sceneDidLoad() {
+        super.sceneDidLoad()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        super.update(currentTime)
+    }
+}
